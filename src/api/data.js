3538de459f8a0c50725bce91c38f148f7aa60d37
Will Fisher <willem.fisher98@gmail.com>
2025-12-22 21:58:04 +0000

    Setting up API and adding content into the compone

--- src/api/data.js
+++ src/api/data.js
@@ -1,14 +1,57 @@
-const base_url = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+const API_key = "c38ce559cd962d9d5e5a2a3872317c08";
 
-const api = "c38ce559cd962d9d5e5a2a3872317c08";
 
-export async function fetchWeatherData(location) {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${api}`)
 
-    if(response.ok) {
+
+
+//This is a function that fetches weather data based on Longitude and Latitude 
+async function fetchWeatherData(lon, lat) {
+    
+    //This uses the longitude and latitude parameters to gather data from a server
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`)
+    
+    //If the data doesn't work, then it will throw an error
+    if(!response.ok) {
         throw new Error("Failed to fetch weather data")
     }
 
+    //If the data does work, then we will store it in the variable named "data".
     const data = await response.json();
+
+    //We return the data. The result of calling this function will result in returning the data requested. Wihtout this, we are just requesting the data from the server and then not using it
     return data;
+
 }
+
+
+
+
+
+
+
+//This is a function that fetches longitude and latitude of a location and then calls the other function with the returned longitde and latitude
+export async function fetchLocationData(location) {
+
+    //This uses the parameter of "location" to return the longitude and latitude of the provided location
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API_key}`)
+
+    //If the data doesn't work, then it will throw an error
+    if(!response.ok) {
+        throw new Error("Failed to fetch location data")
+    }
+
+    //If the data does work, we are storing it in the variable named "data"
+    const data = await response.json();
+    
+    //We then call the other function with the data returned in this function and store the data returned in the other function in a variable called "weather data"
+    const weatherData = await fetchWeatherData(data[0].lon, data[0].lat);
+
+    //I log the data to the console. This allows me to see the structure of the returned data so I can display it
+    console.log(weatherData);
+    console.log("I love you Sam")
+    
+    //This returns the returned data stored in the variable "weatherData"
+    return weatherData;
+}
+
+
--- src/components/weather-component.jsx
+++ src/components/weather-component.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchLocationData } from "../api/data";
 
 function WeatherComponent() {
+
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(null)
+    const [location, setLocation] = useState("London");
+
+    async function callWeatherData() {
+        setIsLoading(true);
+        setHasError(null)
+
+        try {
+            const weatherData = await fetchLocationData(location);
+            setData(weatherData);
+        } catch(e) {
+            setHasError("Something went wrong");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        callWeatherData();
+    }, [location])
+
+
     return (
-        <div></div>
+        <div>
+            <h1>Test</h1>
+            <button>Press me</button>
+        </div>
     )
 }
 
